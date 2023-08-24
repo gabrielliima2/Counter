032f3fa5488f531055c08010a886a9f8a3909bc4
gabrielliima2 <gabrielantuneslima10@gmail.com>
2023-08-24 15:43:16 -0300

    plus 3 points

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let w1 = document.body.querySelector("div#w1")
 let win1 = 0
 let w2 = document.body.querySelector("div#w2")
 let win2 = 0
+let wScreen = document.body.querySelector("div#winScreen")
 function lessTeam1() {
   if (ponto1 > 0) {
     ponto1 -= 1
@@ -19,8 +20,7 @@ function plusTeam1() {
   }
   p1.innerHTML = ponto1
 
-  if (ponto1 == 12) {
-    window.alert("Team 1 Win!")
+  if (ponto1 >= 12) {
     ponto1 = 0
     p1.innerHTML = ponto1
 
@@ -29,6 +29,8 @@ function plusTeam1() {
 
     win1 += 1
     w1.innerHTML = win1
+
+    window.alert("Team 1 Win!")
   }
 }
 
@@ -45,7 +47,7 @@ function plusTeam2() {
   }
   p2.innerHTML = ponto2
 
-  if (ponto2 == 12) {
+  if (ponto2 >= 12) {
     window.alert("team 2 win!")
     ponto2 = 0
     p2.innerHTML = ponto2
@@ -57,12 +59,49 @@ function plusTeam2() {
   }
 }
 
-function reset(){
+function plus3Team1() {
+  if (ponto1 < 12) {
+    ponto1 += 3
+  }
+  p1.innerHTML = ponto1
+
+  if (ponto1 >= 12) {
+    window.alert("Team 1 Win!")
+    ponto1 = 0
+    p1.innerHTML = ponto1
+
+    ponto2 = 0
+    p2.innerHTML = ponto2
+
+    win1 += 1
+    w1.innerHTML = win1
+  }
+}
+
+function plus3Team2() {
+  if (ponto2 < 12) {
+    ponto2 += 3
+  }
+  p2.innerHTML = ponto2
+
+  if (ponto2 >= 12) {
+    window.alert("team 2 win!")
+    ponto2 = 0
+    p2.innerHTML = ponto2
+
+    ponto1 = 0
+    p1.innerHTML = ponto1
+    win2 += 1
+    w2.innerHTML = win2
+  }
+}
+
+function reset() {
   ponto1 = 0
   ponto2 = 0
   win1 = 0
   win2 = 0
-  p1.innerHTML= ponto1
+  p1.innerHTML = ponto1
   p2.innerHTML = ponto2
   w1.innerHTML = win1
   w2.innerHTML = win2
